c3ceb165915ad017fce996cac5f1eb6ca73f9eb8
admin <admin@fmac.local>
2023-01-21 22:30:24 +0300

    Setup django admin

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTest(SimpleTestCase):
+    pass
+    # def test_calc(self):
+    #     res = calc.add(5,6)
+
+    #     self.assertEqual(res, 111)
+
+
+
+
